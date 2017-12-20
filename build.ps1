8913a414a994c5336d1e41566554e4f671641297
mrsalty <matteo.cunietti@gmail.com>
2017-12-20 11:58:42 +0000

    fix nuget proj path in build.ps1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
-dotnet build .\Examples\WebApi\WebApi.csproj
-dotnet build .\Examples\WebApi\WebApi.ContractTests.csproj
-dotnet build .\FluentRestApi\FluentRestApi.csproj
+dotnet build .\src\Examples\WebApi\WebApi.csproj
+dotnet build .\src\Examples\WebApi\WebApi.ContractTests.csproj
+dotnet build .\src\FluentRestApi\FluentRestApi.csproj
 dotnet build .\src\NContract\NContract.csproj
- nuget push ncontract.nupkg -ApiKey qa3c76wngnspqk6w0hsofy21 -Source https://ci.appveyor.com/nuget/mrsalty-ncn6qtjpeii6/api/v2/package
+nuget pack ".\src\NContract\NContract.csproj"
+nuget push *.nupkg -ApiKey qa3c76wngnspqk6w0hsofy21 -Source "https://ci.appveyor.com/nuget/mrsalty-ncn6qtjpeii6/api/v2/package"
