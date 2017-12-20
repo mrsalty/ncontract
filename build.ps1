261265fcef6d225a10ab1d835bd8fc29a2af6b32
mrsalty <matteo.cunietti@gmail.com>
2017-12-20 12:05:55 +0000

    rename to NContract.Nunit

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
-dotnet build .\src\Examples\WebApi\WebApi.csproj
-dotnet build .\src\Examples\WebApi\WebApi.ContractTests.csproj
-dotnet build .\src\FluentRestApi\FluentRestApi.csproj
-dotnet build .\src\NContract\NContract.csproj
+dotnet build ".\src\Examples\WebApi\WebApi.csproj"
+dotnet build ".\src\Examples\WebApi.ContractTests\WebApi.ContractTests.csproj"
+dotnet build ".\src\FluentRestApi\FluentRestApi.csproj"
+dotnet build ".\src\NContract\NContract.csproj"
 nuget pack ".\src\NContract\NContract.csproj"
 nuget push *.nupkg -ApiKey qa3c76wngnspqk6w0hsofy21 -Source "https://ci.appveyor.com/nuget/mrsalty-ncn6qtjpeii6/api/v2/package"
